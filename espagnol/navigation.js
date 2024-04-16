2524da39f262c868a8756063430e079a5ddae195
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2024-04-16 19:27:52 +0800

    Update navigation.js

--- espagnol/navigation.js
+++ espagnol/navigation.js
@@ -4,20 +4,3 @@ var pages = [
   { title: "Las estaciones", url: "https://timothee123456.github.io/espagnol/aa" },
   // Add more pages here as needed
 ];
-
-var navigationMenu = document.getElementById("navigation-menu");
-for (var i = 0; i < pages.length; i++) {
-  var page = pages[i];
-  var listItem = document.createElement("li");
-  var link = document.createElement("a");
-  link.href = page.url;
-  link.textContent = page.title;
-
-  // Add the "active" class to the current page's link
-  if (window.location.pathname === page.url) {
-    link.classList.add("active");
-  }
-
-  listItem.appendChild(link);
-  navigationMenu.appendChild(listItem);
-}

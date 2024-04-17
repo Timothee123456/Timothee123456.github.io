d78a8e24cbcf2ccbb19a4e960a71a3e4ed2e6b9a
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2024-04-17 08:49:38 +0800

    Create navigation.js

---
+++ espagnol/leçons/navigation.js
@@ -0,0 +1,6 @@
+var pages = [
+  { title: "Mascotas", url: "https://timothee123456.github.io/espagnol/a" },
+  { title: "Mes del año", url: "/espagnol/aaa.html" },
+  { title: "Las estaciones", url: "https://timothee123456.github.io/espagnol/aa" },
+  // Add more pages here as needed
+];

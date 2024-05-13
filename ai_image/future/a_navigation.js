814d45c3596c56270bc81999d689ddbf8f3b93e2
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2024-05-13 18:59:26 +0800

    Create a_navigation.js

---
+++ ai_image/future/a_navigation.js
@@ -0,0 +1,13 @@
+var pages = [
+  "https://timothee123456.github.io/ai_image/future/cities(1).html",
+  "https://timothee123456.github.io/ai_image/future/cities(2).html",
+  "https://timothee123456.github.io/ai_image/future/cities(3).html",
+  "https://timothee123456.github.io/ai_image/future/cities(4).html",
+  "https://timothee123456.github.io/ai_image/future/cities(5).html",
+  "https://timothee123456.github.io/ai_image/future/cities(6).html",
+  "https://timothee123456.github.io/ai_image/future/cities(7).html",
+  "https://timothee123456.github.io/ai_image/future/cities(8).html",
+  "https://timothee123456.github.io/ai_image/future/cities(9).html",
+  "https://timothee123456.github.io/ai_image/future/cities(10).html",
+  // Add more pages here as needed
+];

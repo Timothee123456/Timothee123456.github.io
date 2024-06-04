d66fd886c33065483c883b0b0a7d57429441464f
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2024-06-04 08:27:46 +0800

    Update navigation2.js

--- espagnol/leçons/navigation2.js
+++ espagnol/leçons/navigation2.js
@@ -1,13 +1,13 @@
 var pages = [
-  { title: "Día de la semana", url: "https://timothee123456.github.io/espagnol/leçons/jours-de-la-semaine" },
-  { title: "Mes del año", url: "https://timothee123456.github.io/espagnol/leçons/mois" },
-  { title: "Números", url: "https://timothee123456.github.io/espagnol/leçons/nombres" },
-  { title: "Mascotas", url: "https://timothee123456.github.io/espagnol/leçons/animaux" },
-  { title: "Material escolar", url: "https://timothee123456.github.io/espagnol/leçons/materiel-scolaire" },
-  { title: "Verbos", url: "https://timothee123456.github.io/espagnol/leçons/verbes" },
-  { title: "Descriptión", url: "https://timothee123456.github.io/espagnol/leçons/description" },
-  { title: "Comparer", url: "https://timothee123456.github.io/espagnol/leçons/comparer" },
-  { title: "Arbre généalogique", url: "https://timothee123456.github.io/espagnol/leçons/famille" },
-  { title: "Exercices", url: "https://timothee123456.github.io/espagnol/jours-de-la-semaine" },
+  { title: "Día de la semana", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/jours-de-la-semaine" },
+  { title: "Mes del año", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/mois" },
+  { title: "Números", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/nombres" },
+  { title: "Mascotas", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/animaux" },
+  { title: "Material escolar", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/materiel-scolaire" },
+  { title: "Verbos", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/verbes" },
+  { title: "Descriptión", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/description" },
+  { title: "Comparer", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/comparer" },
+  { title: "Arbre généalogique", class: "li", url: "https://timothee123456.github.io/espagnol/leçons/famille" },
+  { title: "Exercices", class: "li", url: "https://timothee123456.github.io/espagnol/jours-de-la-semaine" },
   // Add more pages here as needed
 ];

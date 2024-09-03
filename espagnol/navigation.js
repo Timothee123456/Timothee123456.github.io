441b3ab4e3cd260011fbff70e694b6d158dcbb1e
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2024-09-03 09:08:50 +0800

    Update navigation.js

--- espagnol/navigation.js
+++ espagnol/navigation.js
@@ -25,7 +25,7 @@ function createMenu() {
         { title: "Entrevisto", url: "https://timothee123456.github.io/espagnol/interview" },
         { title: "Asignaturas", url: "https://timothee123456.github.io/espagnol/matières" },
         { title: "Las vacaciones", url: "https://timothee123456.github.io/espagnol/vacances" },
-        { title: "Leçons", url: "https://timothee123456.github.io/espagnol/leçons/jours-de-la-semaine", floatRight: true }
+        { title: "5ème", url: "https://timothee123456.github.io/espagnol/5/materiel-scolaire", floatRight: true }
     ];
 
     // Get the current page title

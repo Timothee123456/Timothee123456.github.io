9998924f9a5017f7f68643191c14e4154fef09c4
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2024-12-10 20:03:03 +0800

    Update and rename eval-content.html to eval-content.js

---
+++ eval/histoire-geo/4/eval-content.js
@@ -0,0 +1,4 @@
+const sites = [
+	{title: 'dates', url: 'https://timothee123456.github.io/eval/histoire-geo/4/dates'},
+<a href="https://timothee123456.github.io/eval/histoire-geo/4/dates" class="myButton">Site pour réviser</a>
+	]

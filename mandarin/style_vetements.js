c07c7bfdbef8e2d0d864a6e60c742487d9985628
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-09-23 19:57:31 +0800

    Create style_vetements.js

---
+++ mandarin/style_vetements.js
@@ -0,0 +1,11 @@
+var flashcards = [
+    { chinese: "衣服", pinyin: "yī fú", french: "Vêtements" },
+    { chinese: "时髦", pinyin: "shí máo", french: "à la mode" },
+    { chinese: "有型", pinyin: "yǒu xíng", french: "avoir du style" },
+    { chinese: "有美感", pinyin: "yǒu měi gǎn", french: "avoir le sens de la beauté" },
+    { chinese: "干净", pinyin: "gān jìng", french: "propre" },
+    { chinese: "随意", pinyin: "suí yì", french: "fraytale" },
+    { chinese: "穿", pinyin: "chuān", french: "s'habiller" },
+    { chinese: "戴", pinyin: "dài", french: "mettre des accessoires" },
+    { chinese: "穿搭", pinyin: "chuān dā", french: "outfit" },
+];

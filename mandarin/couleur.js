b156f686c6e687e1d8d7a0f9bd9683fcccef3ed3
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-09-24 14:56:29 +0800

    Create couleur.js

---
+++ mandarin/couleur.js
@@ -0,0 +1,24 @@
+var title = {chinese: "颜色", pinyin: "yán sè", french: "couleurs"}
+
+var flashcards = [
+{chinese: "颜色(的)", pinyin: "yán sè", french: "couleurs"},
+{chinese: "黑色", pinyin: "hēi sè", french: "noir"},
+{chinese: "白色", pinyin: "bái sè", french: "blanc"},
+{chinese: "红色", pinyin: "hóng sè", french: "rouge"},
+{chinese: "绿色", pinyin: "lǜ sè", french: "vert"},
+{chinese: "蓝色", pinyin: "lán sè", french: "bleu"},
+{chinese: "黄色", pinyin: "huáng sè", french: "jaune"},
+{chinese: "咖啡色", pinyin: "kāfēi sè", french: "couleur du café"},
+{chinese: "棕色", pinyin: "zōng sè", french: "marron"},
+{chinese: "灰色", pinyin: "huī sè", french: "gris"},
+{chinese: "紫色", pinyin: "zǐ sè", french: "violet"},
+{chinese: "橙色", pinyin: "chéng sè", french: "orange"},
+{chinese: "粉红色", pinyin: "fěn hóng sè", french: "rose"},
+{chinese: "金色", pinyin: "jīn sè", french: "or"},
+{chinese: "银色", pinyin: "yín sè", french: "argent"},
+{chinese: "彩色", pinyin: "cǎi sè", french: "multicolore"},
+{chinese: "彩虹", pinyin: "cǎihóng", french: "arc-en-ciel"},
+{chinese: "肤色", pinyin: "fū sè", french: "couleur de la peau"},
+{chinese: "深", pinyin: "shēn", french: "couleur foncée"},
+{chinese: "浅", pinyin: "qiǎn", french: "couleur claire"}
+];

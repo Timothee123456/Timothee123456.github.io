8bf3da0feec26b76fda919d81d9ca6a8a61f81fa
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-09-24 15:02:20 +0800

    Create localisateurs.js

---
+++ mandarin/localisateurs.js
@@ -0,0 +1,16 @@
+var title = {chinese: "", pinyin: "", french: "Localisateurs"}
+
+var flashcards = [
+{chinese: "上", pinyin: "shàng", french: "dessus"},
+{chinese: "下", pinyin: "xià", french: "dessous"},
+{chinese: "前", pinyin: "qián", french: "devant"},
+{chinese: "后", pinyin: "hòu", french: "derrière"},
+{chinese: "左", pinyin: "zuǒ", french: "à gauche"},
+{chinese: "右", pinyin: "yòu", french: "à droite"},
+{chinese: "里", pinyin: "lǐ", french: "dedans"},
+{chinese: "外", pinyin: "wài", french: "dehors"},
+{chinese: "旁边", pinyin: "páng biān", french: "à côté"},
+{chinese: "对面", pinyin: "duì miàn", french: "en face"},
+{chinese: "中间", pinyin: "zhōng jiān", french: "au milieu"},
+{chinese: "附近", pinyin: "fù jìn", french: "dans les environs"}
+];

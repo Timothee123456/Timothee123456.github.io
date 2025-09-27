2e6c6d230a6650867e273a39d30b379507644ff1
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-09-27 16:51:56 +0800

    Create temp.js

---
+++ flashcards/mandarin/temp.js
@@ -0,0 +1,11 @@
+var title = {chinese: "身体", pinyin: "shēnti", french: "corps"}
+
+var flashcards = [
+  { chinese: "身体", pinyin: "shēnti", french: "corps" },
+  { chinese: "耳朵", pinyin: "ěrduo", french: "oreille" },
+  { chinese: "脚", pinyin: "jiǎo", french: "pied" },
+  { chinese: "背", pinyin: "bèi", french: "dos" },
+  { chinese: "头发", pinyin: "tóufà", french: "cheveux" },
+  { chinese: "手", pinyin: "shǒu", french: "main" },
+  { chinese: "脸", pinyin: "liǎn", french: "visage" },
+];

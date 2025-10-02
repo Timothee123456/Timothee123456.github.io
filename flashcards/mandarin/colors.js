632df41fba2f8b5165d28cc3c6752a4f274945d5
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-02 16:20:45 +0800

    Update colors.js

--- flashcards/mandarin/colors.js
+++ flashcards/mandarin/colors.js
@@ -15,3 +15,21 @@ const lightColors = [
     '#FFEFDB', // Honeydew
     '#FFF0F5', // LavenderBlush
   ];
+
+const darkColors = [
+      '#004f94', // Dark AliceBlue
+      '#7c4d10', // Dark AntiqueWhite
+      '#009494', // Dark Azure
+      '#6c6c1f', // Dark Beige
+      '#874900', // Dark Bisque
+      '#8e7200', // Dark Cornsilk
+      '#8c5700', // Dark PapayaWhip
+      '#8a7c00', // Dark LemonChiffon
+      '#784818', // Dark Linen
+      '#909000', // Dark LightYellow
+      '#009090', // Dark LightCyan
+      '#970000', // Dark Snow
+      '#00964b', // Dark MintCream
+      '#8e4f00', // Dark Honeydew
+      '#940031', // Dark LavenderBlush
+    ];

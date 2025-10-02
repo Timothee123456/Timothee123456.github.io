a0a3599fabd405c95e0d788c722757765b497fdf
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-02 15:35:55 +0800

    Create colors.js

---
+++ flashcards/mandarin/colors.js
@@ -0,0 +1,17 @@
+const lightColors = [
+    '#F0F8FF', // AliceBlue
+    '#FAEBD7', // AntiqueWhite
+    '#F0FFFF', // Azure
+    '#F5F5DC', // Beige
+    '#FFE4C4', // Bisque
+    '#FFF8DC', // Cornsilk
+    '#FFEFD5', // PapayaWhip
+    '#FFFACD', // LemonChiffon
+    '#FAF0E6', // Linen
+    '#FFFFE0', // LightYellow
+    '#E0FFFF', // LightCyan
+    '#FFFAFA', // Snow
+    '#F5FFFA', // MintCream
+    '#FFEFDB', // Honeydew
+    '#FFF0F5', // LavenderBlush
+  ];

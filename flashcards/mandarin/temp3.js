e713301125b349915050a5505ec77e41b5115dd0
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-05 18:03:16 +0800

    Create temp3.js

---
+++ flashcards/mandarin/temp3.js
@@ -0,0 +1,12 @@
+export var title = "Personnalité"
+
+export var flashcards = [
+{ chinese: "性格", pinyin: "xìng gé", french: "personnalité" },
+{ chinese: "风格", pinyin: "fēng gé", french: "style" },
+{ chinese: "酷", pinyin: "kù", french: "cool" },
+{ chinese: "冷", pinyin: "lěng", french: "froid" },
+{ chinese: "爱玩儿", pinyin: "ài wánr", french: "aimer jouer" },
+{ chinese: "爱开玩笑", pinyin: "ài kāiwánxiào", french: "aimer faire des blagues" },
+{ chinese: "自然", pinyin: "zìrán", french: "naturel" },
+{ chinese: "假", pinyin: "jiǎ", french: "faux" },
+];

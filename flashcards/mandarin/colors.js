01ff388aea53fb2fec5977e46627e22c5261737b
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-07 11:58:19 +0800

    Update colors.js

--- flashcards/mandarin/colors.js
+++ flashcards/mandarin/colors.js
@@ -1,35 +1,36 @@
 const lightColors = [
-    '#F0F8FF', // AliceBlue
-    '#FAEBD7', // AntiqueWhite
-    '#F0FFFF', // Azure
-    '#F5F5DC', // Beige
-    '#FFE4C4', // Bisque
-    '#FFF8DC', // Cornsilk
-    '#FFEFD5', // PapayaWhip
-    '#FFFACD', // LemonChiffon
-    '#FAF0E6', // Linen
-    '#FFFFE0', // LightYellow
-    '#E0FFFF', // LightCyan
-    '#FFFAFA', // Snow
-    '#F5FFFA', // MintCream
-    '#FFEFDB', // Honeydew
-    '#FFF0F5', // LavenderBlush
-  ];
+      "#FAEBD7", // AntiqueWhite
+      "#FFFFE0", // LightYellow
+      "#F0F8FF", // AliceBlue
+      "#FFE4C4", // Bisque
+      "#E0FFFF", // LightCyan
+      "#F0FFFF", // Azure
+      "#FFF8DC", // Cornsilk
+      "#F5F5DC", // Beige
+      "#FFEFD5", // PapayaWhip
+      "#FAF0E6", // Linen
+      "#FFFACD", // LemonChiffon
+      "#FFFAFA", // Snow
+      "#FFEFDB", // Honeydew
+      "#F5FFFA", // MintCream
+      "#FFF0F5", // LavenderBlush
+    ];
+
 
 const darkColors = [
-      '#004f94', // Dark AliceBlue
       '#7c4d10', // Dark AntiqueWhite
-      '#009494', // Dark Azure
-      '#6c6c1f', // Dark Beige
-      '#874900', // Dark Bisque
-      '#8e7200', // Dark Cornsilk
-      '#8c5700', // Dark PapayaWhip
-      '#8a7c00', // Dark LemonChiffon
-      '#784818', // Dark Linen
       '#909000', // Dark LightYellow
+      '#004f94', // Dark AliceBlue
+      '#874900', // Dark Bisque
       '#009090', // Dark LightCyan
+      '#009494', // Dark Azure
+      '#8e7200', // Dark Cornsilk
+      '#6c6c1f', // Dark Beige
+      '#8c5700', // Dark PapayaWhip
+      '#784818', // Dark Linen
+      '#8a7c00', // Dark LemonChiffon
       '#970000', // Dark Snow
-      '#00964b', // Dark MintCream
       '#8e4f00', // Dark Honeydew
+      '#00964b', // Dark MintCream
       '#940031', // Dark LavenderBlush
     ];

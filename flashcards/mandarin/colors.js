9d14c3a64d55ab21db85d73fc08d27f87c9963b9
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-08 04:51:35 +0000

    add export in colors.js

--- flashcards/mandarin/colors.js
+++ flashcards/mandarin/colors.js
@@ -1,4 +1,4 @@
-const lightColors = [
+export const lightColors = [
       '#F0F8FF', // AliceBlue
       '#FAEBD7', // AntiqueWhite
       '#F0FFFF', // Azure
@@ -16,20 +16,20 @@ const lightColors = [
       '#FFF0F5', // LavenderBlush
     ];
   
-  const darkColors = [
-        '#004f94', // Dark AliceBlue
-        '#7c4d10', // Dark AntiqueWhite
-        '#009494', // Dark Azure
-        '#6c6c1f', // Dark Beige
-        '#874900', // Dark Bisque
-        '#8e7200', // Dark Cornsilk
-        '#8c5700', // Dark PapayaWhip
-        '#8a7c00', // Dark LemonChiffon
-        '#784818', // Dark Linen
-        '#909000', // Dark LightYellow
-        '#009090', // Dark LightCyan
-        '#970000', // Dark Snow
-        '#00964b', // Dark MintCream
-        '#8e4f00', // Dark Honeydew
-        '#940031', // Dark LavenderBlush
-      ];
+export const darkColors = [
+      '#004f94', // Dark AliceBlue
+      '#7c4d10', // Dark AntiqueWhite
+      '#009494', // Dark Azure
+      '#6c6c1f', // Dark Beige
+      '#874900', // Dark Bisque
+      '#8e7200', // Dark Cornsilk
+      '#8c5700', // Dark PapayaWhip
+      '#8a7c00', // Dark LemonChiffon
+      '#784818', // Dark Linen
+      '#909000', // Dark LightYellow
+      '#009090', // Dark LightCyan
+      '#970000', // Dark Snow
+      '#00964b', // Dark MintCream
+      '#8e4f00', // Dark Honeydew
+      '#940031', // Dark LavenderBlush
+    ];

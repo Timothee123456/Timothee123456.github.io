df7656d1917a24f453af052ca469122fa9e833a3
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-18 18:25:45 +0800

    Create th1ch1.js

---
+++ definitions/histoire-geo/sets/th1ch1.js
@@ -0,0 +1,18 @@
+export var title = "Thème 1 : Dynamiques territoriales de la France contemporaine"
+export var date = "2025-10-18"
+
+export var vocabularyWords = [
+    { word: "Héliotropisme", definition: "attraction pour les régions plus ensoleillées." },
+    { word: "Accroissement naturel", definition: "nombre de naissances supérieur à celui des décès." },
+    { word: "Solde migratoire", definition: "différence entre nombre d'entrées et de sorties dans un territoire." },
+    { word: "Étalement urbain", definition: "extension de l'aire urbaine dans des espaces ruraux due à la croissance de la population." },
+    { word: "Migration pendulaire", definition: "déplacement quotidien entre le domicile et le travail." },
+    { word: "Périurbanisation", definition: "extension de plus en plus lointaine de l'habitat urbain avec de nouvelles habitudes et de nouvelles façons de vivre." },
+    { word: "Gentrification", definition: "une forme particulière d'embourgeoisement d'un espace populaire qui passe par la transformation de l'habitat, des commerces ou de l'espace public. Il s'agit d'une transformation sociale qui se traduit par une transformation matérielle et symbolique de l'espace." },
+    { word: "Ville-centre (commune-centre)", definition: "Commune centrale (dans ses limites administratives) d'une agglomération. Partie centrale de l'aire urbaine, la commune la plus peuplée, qui concentre la plupart des emplois." },
+    { word: "Banlieue", definition: "ensemble des agglomérations qui entourent une grande ville. Elle correspond à l'espace urbanisé situé à la périphérie d'une ville centre et dépendant de celle-ci pour les emplois, les services (notamment les commerces) et les transports." },
+    { word: "Couronne périurbaine", definition: "(communes plus éloignées mais dont au moins 40% des habitants vont travailler dans le reste de l'aire urbaine, donc la ville centre OU la banlieue)." },
+    { word: "Urbanisation", definition: "Augmentation de la population urbaine et transformation de l'espace rural en espace urbain." },
+    { word: "Mondialisation", definition: "Mise en relation des différentes parties du monde grâce aux échanges." },
+    { word: "Métropole", definition: "Ensemble urbain de grande importance qui exerce des fonctions de commandement, d'organisation, d'impulsion sur une région et qui permet son intégration avec le reste du monde. Elle concentre des fonctions économiques, politiques et culturelles." }
+];

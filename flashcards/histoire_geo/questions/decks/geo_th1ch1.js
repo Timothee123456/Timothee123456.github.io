d04fec761fd784053978eb07d4a9abd0ae99f90a
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-20 17:38:52 +0800

    Add new geography flashcards to geo_th1ch1.js

--- flashcards/histoire_geo/questions/decks/geo_th1ch1.js
+++ flashcards/histoire_geo/questions/decks/geo_th1ch1.js
@@ -3,4 +3,19 @@ export var date = "2025-09-12"
 
 export var flashcards = [
     { question: "Quel est la proportion de français qui sont urbains ?", answer: "4 sur 5 sont urbains" },
+    { question: "Combien d'aires urbaines il y a t-il en France ?", answer: "354 aires urbaines" },
+    { question: "Quel pourcentage de la population française vit sous l'influence d'une ville ?", answer: "93%" },
+    { question: "Les 10 premières aires urbaines de France", answer: "Paris, Lyon, Marseille, Lille, Strasbourg, Rennes, Nantes, Bordeaux, Toulouse, Nice" },
+    { question: "Expliquer les problèmes et inégalités de la banlieue", answer: "moin de revenus, plus de chômage, présence de violence et tensions" },
+    { question: "Quels sont les trois parties d'une aire urbaine ?", answer: "La ville centre, puis la banlieue, et les communes les plus éloignés font partie de la couronne périurbaine" },
+    { question: "L'urbanisation induit une plus forte connexion des grandes aires urbaines entre elles par quoi ?", answer: "Par des réseaux de transports (autoroutes, lignes TGV)" },
+    { question: "Toulouse se spécialise dans quel domaine ?", answer: "L'aérospatiale" },
+    { question: "Lyon se spécialise dans quels domaines (3) ?", answer: "La chimie, la transition énergétique et les biotecnologies" },
+    { question: "Quel est la principale cause de l'urbanisation du territoire français ?", answer: "L'exode rural" },
+    { question: "Depuis quel année, la majorité de la population vit-elle en ville ?", answer: "Depuis 1931" },
+    { question: "Lyon est-elle une métropole ?", answer: "Oui" },
+    { question: "Lyon est le combientième pôle économique de France ?", answer: "Le 2ème après Paris" },
+    { question: "Pourquoi les habitants s'installent-ils dans la banlieue ou l'espace périurbain plutôt que dans le centre ?", answer: "Les loyers sont très chers au centre et les espaces disponibles diminuent" },
+    { question: "Pourquoi les grandes aires urbaines sont elles attractives (3) ?", answer: "Elles sont attractivent grace à leurs activités économiques, la concentration de leurs services (hopitaux, école...) et la qualité de leur connexion aux réseaux de transports" },
+
 ];

75f8c89d5986651e6ab5b10a297ec03ecdcb5c2b
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-27 09:49:21 +0800

    Create ville.js

---
+++ flashcards/mandarin/decks/ville.js
@@ -0,0 +1,15 @@
+var title = "Ville"
+var date = "2023-01-01"
+
+var flashcards = [
+{chinese: "饭店", pinyin: "fàn diàn", french: "restaurant" },
+{ chinese: "邮局", pinyin: "yóu jú", french: "poste" },
+{ chinese: "电影院", pinyin: "diàn yǐng yuàn", french: "cinéma" },
+{ chinese: "商店", pinyin: "shāng diàn", french: "magasin" },
+{ chinese: "超市 = 超级市场", pinyin: "chāo shì = chāo jí shì chǎng", french: "supermarché" },
+{ chinese: "教堂", pinyin: "jiào táng", french: "église" },
+{ chinese: "百货公司", pinyin: "bǎi huò gōng sī", french: "grand magasin" },
+{ chinese: "市政大楼", pinyin: "shì zhèng dà lóu", french: "administration" },
+{ chinese: "银行", pinyin: "yín háng", french: "banque" },
+{ chinese: "市中心", pinyin: "shì zhōng xīn", french: "centre-ville" }
+];

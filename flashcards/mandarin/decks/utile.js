a950b226839eacbf885a6ef77a06ec9b4bf3cc32
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-28 17:13:47 +0800

    Create utile.js

---
+++ flashcards/mandarin/decks/utile.js
@@ -0,0 +1,13 @@
+var title = "Mots Chinois Utiles";
+var date = "2025-10-28";
+
+var flashcards = [
+{chinese: "你好", pinyin: "nǐ hǎo", french: "bonjour", title: title},
+{chinese: "谢谢", pinyin: "xiè xiè", french: "merci", title: title},
+{chinese: "不客气", pinyin: "bù kè qì", french: "de rien", title: title},
+{chinese: "再见", pinyin: "zài jiàn", french: "au revoir", title: title},
+{chinese: "请问", pinyin: "qǐng wèn", french: "excusez-moi", title: title},
+{chinese: "对不起", pinyin: "duì bù qǐ", french: "pardon", title: title},
+{chinese: "多少钱", pinyin: "duō shao qián", french: "combien ça coûte ?", title: title},
+{chinese: "厕所在哪里", pinyin: "cè suǒ zài nǎ lǐ", french: "où sont les toilettes ?", title: title}
+];

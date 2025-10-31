6b3c238395274faf7b00b6aa705816ee67a69990
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-10-31 02:23:46 +0000

    add hsk part 1 & 2

---
+++ flashcards/mandarin/decks/hsk2p1.js
@@ -0,0 +1,26 @@
+export var title = "HSK 2 part 1";
+export var date = "2025-10-31";
+
+export var flashcards = [
+{ chinese: "您", pinyin: "nín", french: "you" },
+{ chinese: "它", pinyin: "tā", french: "it" },
+{ chinese: "大家", pinyin: "dàjiā", french: "everyone" },
+{ chinese: "每", pinyin: "měi", french: "every" },
+{ chinese: "为什么", pinyin: "wèishénme", french: "why" },
+{ chinese: "零", pinyin: "líng", french: "zero" },
+{ chinese: "两", pinyin: "liǎng", french: "two" },
+{ chinese: "百", pinyin: "bǎi", french: "hundred" },
+{ chinese: "千", pinyin: "qiān", french: "thousand" },
+{ chinese: "第一", pinyin: "dìyī", french: "first" },
+{ chinese: "次", pinyin: "cì", french: "time" },
+{ chinese: "件", pinyin: "jiàn", french: "piece" },
+{ chinese: "别", pinyin: "bié", french: "don't" },
+{ chinese: "非常", pinyin: "fēicháng", french: "very" },
+{ chinese: "也", pinyin: "yě", french: "also" },
+{ chinese: "还", pinyin: "hái", french: "still" },
+{ chinese: "最", pinyin: "zuì", french: "most" },
+{ chinese: "真", pinyin: "zhēn", french: "really" },
+{ chinese: "正在", pinyin: "zhèngzài", french: "in the process of" },
+{ chinese: "已经", pinyin: "yǐjīng", french: "already" },
+{ chinese: "一起", pinyin: "yìqǐ", french: "together" },
+]
---
+++ flashcards/mandarin/decks/hsk2p2.js
@@ -0,0 +1,26 @@
+export var title = "HSK 2 part 2";
+export var date = "2025-10-31";
+
+export var flashcards = [
+{ chinese: "再", pinyin: "zài", french: "again" },
+{ chinese: "就", pinyin: "jiù", french: "then" },
+{ chinese: "因为……所以……", pinyin: "yīnwèi…suǒyǐ…", french: "because...so..." },
+{ chinese: "虽然……但是……", pinyin: "suīrán…dànshì…", french: "although...but..." },
+{ chinese: "从", pinyin: "cóng", french: "from" },
+{ chinese: "比", pinyin: "bǐ", french: "compare" },
+{ chinese: "往", pinyin: "wǎng", french: "to" },
+{ chinese: "离", pinyin: "lí", french: "away from" },
+{ chinese: "得", pinyin: "de", french: "(structural particle)" },
+{ chinese: "着", pinyin: "zhe", french: "(aspect particle)" },
+{ chinese: "过", pinyin: "guò", french: "(experience particle)" },
+{ chinese: "吧", pinyin: "ba", french: "(modal particle)" },
+{ chinese: "机场", pinyin: "jīchǎng", french: "airport" },
+{ chinese: "教室", pinyin: "jiàoshì", french: "classroom" },
+{ chinese: "房间", pinyin: "fángjiān", french: "room" },
+{ chinese: "路", pinyin: "lù", french: "road" },
+{ chinese: "左边", pinyin: "zuǒbiān", french: "left" },
+{ chinese: "右边", pinyin: "yòubiān", french: "right" },
+{ chinese: "外", pinyin: "wài", french: "outside" },
+{ chinese: "旁边", pinyin: "pángbiān", french: "side" },
+{ chinese: "一下", pinyin: "yīxià", french: "briefly" }
+]

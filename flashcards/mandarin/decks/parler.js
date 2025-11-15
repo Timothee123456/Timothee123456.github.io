027e0af51121f0de38e89b53fa8bcb76c09a20df
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-11-15 12:48:20 +0800

    Create parler.js

---
+++ flashcards/mandarin/decks/parler.js
@@ -0,0 +1,22 @@
+export var title = "Parler"
+export var date = "2025-11-15"
+
+export var flashcards = [
+{chinese: "告诉", pinyin: "gàosù", french: "raconter"},
+{chinese: "谈恋爱", pinyin: "tán liànài", french: "sortir ensemble"},
+{chinese: "谈一谈", pinyin: "tányìtán", french: "discuter sérieusement"},
+{chinese: "说坏话", pinyin: "shuō huàihuà", french: "parler du mal des gens"},
+{chinese: "不要说", pinyin: "búyào shuō", french: "ne parle pas"},
+{chinese: "上课说话", pinyin: "shàngkè shuōhuà", french: "parler en cours"},
+{chinese: "告诉我们", pinyin: "gàosù wǒmen", french: "nous raconter"},
+{chinese: "我告诉你一件事", pinyin: "wǒ gàosù nǐ yī jiàn shì", french: "je te raconte une affaire"},
+{chinese: "什么事情? 没事!", pinyin: "shénme shìqing? méishi!", french: "quelle affaire ? il n'y en a pas"},
+{chinese: "我没有/忘记 这件事情。", pinyin: "wǒ méiyǒu / wàng jì zhè jiàn shìqing", french: "je n'ai pas oublié cette affaire"},
+{chinese: "敢不敢? 我敢", pinyin: "gǎn bùgǎn? wǒ gǎn", french: "oses-tu ? j'ose"},
+{chinese: "妈妈发火了。", pinyin: "māmā fāhuǒ le", french: "maman est énervée"},
+{chinese: "她非常生气", pinyin: "tā fēicháng shēngqì", french: "elle est souvent énervée contre moi"},
+  chinese: "她非常生气", pinyin: "tā fēicháng shēngqì", french: "elle est souvent énervée contre moi"},
+{chinese: "他是个小气的人,而且自私,爱说谎,不用功。我很讨厌他。", pinyin: "tā shì gè xiǎoqi de rén, érqiě zìsī, ài shuōhuǎng, bù yònggōng. wǒ hěn tǎoyàn tā.", french: "il est radin, égoïste, dit des mensonges et ne travaille pas. Je le déteste."},
+{chinese: "他是一个热心的人,他爱帮助别人。而且他自信,用功,对朋友", pinyin: "tā shì yī gè rèxīn de rén, tā ài bāngzhù biérén. érqiě tā zìxìn, yònggōng, duì péng you", french: "il est un homme chaleureux, il aime aider les autres. de plus, il est confiant, travailleur et bon ami."},
+{chinese: "好。", pinyin: "hão", french: "bien."},
+];

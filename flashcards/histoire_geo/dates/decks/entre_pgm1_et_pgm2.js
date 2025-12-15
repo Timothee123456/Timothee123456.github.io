c9b076ceabf2bfa1817ad176be32eff4e471784b
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2025-12-15 18:49:34 +0800

    Update entre_pgm1_et_pgm2.js

--- flashcards/histoire_geo/dates/decks/entre_pgm1_et_pgm2.js
+++ flashcards/histoire_geo/dates/decks/entre_pgm1_et_pgm2.js
@@ -2,5 +2,17 @@ export var title = "L'entre deux guerres"
 export var date = "2025-12-15"
 
 export var flashcards = [
+    { date: "Jeudi 24 octobre 1929", event: "Krach boursier aux États-Unis (Jeudi noir)" },
+    { date: "8 novembre 1923", event: "Coup d'État d'Hitler" },
+    { date: "30 janvier 1933", event: "Hitler devient chancelier" },
+    { date: "27 février 1933", event: "Incendire du Reichstag" },
+    { date: "Août 1934", event: "Mort du président Hindenburg" },
+    { date: "1925", event: "Mein Kampf - année de publication" },
+    { date: "Septembre 1935", event: "Lois de Nuremberg" },
+    { date: "1936", event: "Remilitarisation de la Rhénanie" },
+    { date: "Mars 1938", event: "Annexation de l'Autriche (L'Anschluss)" },
     { date: "", event: "" },
+    { date: "1938", event: "Annexation des Sudètes" },
+    { date: "septembre 1938", event: "Accords de Munich" },
+    { date: "1 septembre 1939", event: "Invasion de la Pologne - Début de le Seconde Guerre mondiale" },
 ];

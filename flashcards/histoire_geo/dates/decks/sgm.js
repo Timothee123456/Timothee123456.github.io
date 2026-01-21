3e6e77e09eae752187e854d3ccb0e9ffbb6dec01
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-01-21 10:13:12 +0800

    Update sgm.js

--- flashcards/histoire_geo/dates/decks/sgm.js
+++ flashcards/histoire_geo/dates/decks/sgm.js
@@ -7,6 +7,6 @@ export var flashcards = [
     { date: "1941", event: "Attaque des Japonais sur Pearl Harbor" },
     { date: "6 juin 1944", event: "Débarquement en Normandie" },
     { date: "avril 1945", event: "Hitler se suicide" },
-    { date: "8 mai 1945", event: "Fin de la guerre en Europe" },
+    { date: "8 mai 1945", event: "Capitualtion de l'Allemagne - Fin de la guerre en Europe" },
     { date: "6 et 9 août 1945", event: "Les villes japonaises d'Hiroshima et Nagasaki se font bombardés par les États-Unis - Fin de la 2ème guerre mondiale" },
 ];

3ecf7aa3626ec5bf0f45e00a02f6acbf24a373a2
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-02-09 10:31:57 +0800

    Create act2.js

---
+++ definitions/sia/sets/act2.js
@@ -0,0 +1,25 @@
+export var title = "Vocabulary List Act II Julius Caesar";
+export var number = 8
+
+export var vocabularyWords = [
+    { word: "Liable", definition: "Legally responsible; subject or susceptible; likely or apt" },
+    { word: "Countenance", definition: "Appearance, especially the look or expression of the face; approval or favor" },
+    { word: "Ambition", definition: "An earnest desire for some type of achievement, distinction, power, honor, fame, or wealth, and the willingness to strive for its attainment." },
+    { word: "Redress", definition: "The making right of what is wrong; relief from wrong or injury" },
+    { word: "Profess", definition: "To lay claim to, often insincerely; pretend to; to declare openly; announce or affirm" },
+    { word: "Instigate", definition: "To urge, provoke, or incite to some action or course" },
+    { word: "Entrails", definition: "The internal parts of the trunk of an animal body; the intestines" },
+    { word: "Dismember", definition: "To take away limbs; divide limb from limb" },
+    { word: "Insurrection", definition: "A violent uprising against an authority or government." },
+    { word: "Commend", definition: "To present, mention, or praise as worthy of confidence, notice, and kindness" },
+    { word: "Disperse", definition: "To drive or send off in various directions; scatter" },
+    { word: "Loins", definition: "The part of the body on either side of the spine between the false ribs and hipbone." },
+    { word: "Disclose", definition: "To make known; reveal or uncover; to cause to appear; allow to be seen" },
+    { word: "Petition", definition: "A formal request, often bearing the names of a number of those making a request." },
+    { word: "Contagion", definition: "The communication of disease by direct or indirect contact" },
+    { word: "Emulate", definition: "To try to equal or excel; imitate with effort to equal or surpass" },
+    { word: "Yearn", definition: "To have an earnest or strong desire; long; to feel tenderness; be moved or attracted" },
+    { word: "Beseech", definition: "To implore urgently; to beg eagerly for; solicit" },
+    { word: "Disjoin", definition: "To undo or prevent the junction or union of; disunite; separate" },
+    { word: "Ascend", definition: "To move, climb or go upward; mount; rise; to slant upward." }
+];

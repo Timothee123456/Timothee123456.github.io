cd79f93faa3e443e92bb65e487041315c4b78e53
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-04-12 04:31:16 +0530

    Update atout_lire_vetements.js

--- flashcards/mandarin/decks/atout_lire_vetements.js
+++ flashcards/mandarin/decks/atout_lire_vetements.js
@@ -21,5 +21,60 @@ export var flashcards = [
   { chinese: "袜子", pinyin: "wà zi", french: "chaussettes" },
     {chinese: "帽子", pinyin: "mào zi", french: "chapeau/casquette"},
     {chinese: "围巾", pinyin: "wéi jīn", french: "écharpe"},
-    {chinese: "领带", pinyin: "lǐng dài", french: "cravate"}
+    {chinese: "领带", pinyin: "lǐng dài", french: "cravate"},
+    {
+    "chinese": "棉",
+    "pinyin": "mián",
+    "french": "coton"
+  },
+  {
+    "chinese": "丝绸",
+    "pinyin": "sī chóu",
+    "french": "soie"
+  },
+  {
+    "chinese": "合身",
+    "pinyin": "hé shēn",
+    "french": "ajusté"
+  },
+  {
+    "chinese": "紧",
+    "pinyin": "jǐn",
+    "french": "moulant"
+  },
+  {
+    "chinese": "时髦",
+    "pinyin": "shí máo",
+    "french": "à la mode"
+  },
+  {
+    "chinese": "正装",
+    "pinyin": "zhèng zhuāng",
+    "french": "tenue formelle"
+  },
+  {
+    "chinese": "休闲",
+    "pinyin": "xiū xián",
+    "french": "décontracté"
+  },
+  {
+    "chinese": "清新",
+    "pinyin": "qīng xīn",
+    "french": "style de fraîcheur"
+  },
+  {
+    "chinese": "脱",
+    "pinyin": "tuō",
+    "french": "enlever"
+  },
+  {
+    "chinese": "试穿",
+    "pinyin": "shì chuān",
+    "french": "essayer"
+  },
+  {
+    "chinese": "换",
+    "pinyin": "huàn",
+    "french": "changer"
+  },
 ];

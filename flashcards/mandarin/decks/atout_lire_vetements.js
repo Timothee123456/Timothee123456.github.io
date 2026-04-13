379e53830f050b7fa8f8263618f50d7659061645
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-04-13 15:34:48 +0800

    Update atout_lire_vetements.js

--- flashcards/mandarin/decks/atout_lire_vetements.js
+++ flashcards/mandarin/decks/atout_lire_vetements.js
@@ -4,77 +4,52 @@ export var date = "2026-04-11";
 export var flashcards = [
   { chinese: "内衣", pinyin: "nèi yī", french: "sous-vêtements" },
   { chinese: "睡衣", pinyin: "shuì yī", french: "pyjama" },
-  { chinese: "棉袄", pinyin: "mián ǎo", french: "veste matelassée courte" },
-  { chinese: "羽绒服", pinyin: "yǔ róng fú", french: "doudoune en duvet" },
+  { chinese: "体恤衫", pinyin: "T xù shān", french: "t-shirt" },
+  { chinese: "衬衫", pinyin: "chèn shān", french: "chemise" },
   { chinese: "背心", pinyin: "bèi xīn", french: "débardeur" },
-  { chinese: "夹克", pinyin: "jiá kè", french: "blouson/veste" },
   { chinese: "卫衣", pinyin: "wèi yī", french: "sweat à capuche" },
   { chinese: "连衣裙", pinyin: "lián yī qún", french: "robe" },
+  { chinese: "帽子", pinyin: "mào zi", french: "chapeau/casquette" },
+  { chinese: "手套", pinyin: "shǒu tào", french: "gants" },
+  { chinese: "皮带", pinyin: "pí dài", french: "ceinture" },
+];
+
+
+  
+  
+  
+  
+  
+  
+  
+[
+  { chinese: "毛衣", pinyin: "máo yī", french: "pull" },
+  { chinese: "运动衣", pinyin: "yùn dòng yī", french: "sweat-shirt" },
+  { chinese: "短裤", pinyin: "duǎn kù", french: "short" },
+  { chinese: "长裤", pinyin: "cháng kù", french: "pantalon long" },
+  { chinese: "运动裤", pinyin: "yùn dòng kù", french: "pantalon de sport" },
   { chinese: "半身裙", pinyin: "bàn shēn qún", french: "jupe" },
   { chinese: "紧身裤", pinyin: "jǐn shēn kù", french: "legging" },
   { chinese: "西装", pinyin: "xī zhuāng", french: "costume" },
-  { chinese: "运动服", pinyin: "yùn dòng fú", french: "vêtement de sport" },
   { chinese: "睡衣", pinyin: "shuì yī", french: "pyjamas" },
+  { chinese: "皮鞋", pinyin: "pí xié", french: "chaussures en cuir" },
   { chinese: "凉鞋", pinyin: "liáng xié", french: "sandales" },
   { chinese: "拖鞋", pinyin: "tuō xié", french: "pantoufles" },
   { chinese: "靴子", pinyin: "xuē zi", french: "bottes" },
   { chinese: "袜子", pinyin: "wà zi", french: "chaussettes" },
-    {chinese: "帽子", pinyin: "mào zi", french: "chapeau/casquette"},
-    {chinese: "围巾", pinyin: "wéi jīn", french: "écharpe"},
-    {chinese: "领带", pinyin: "lǐng dài", french: "cravate"},
-    {
-    "chinese": "棉",
-    "pinyin": "mián",
-    "french": "coton"
-  },
-  {
-    "chinese": "丝绸",
-    "pinyin": "sī chóu",
-    "french": "soie"
-  },
-  {
-    "chinese": "合身",
-    "pinyin": "hé shēn",
-    "french": "ajusté"
-  },
-  {
-    "chinese": "紧",
-    "pinyin": "jǐn",
-    "french": "moulant"
-  },
-  {
-    "chinese": "时髦",
-    "pinyin": "shí máo",
-    "french": "à la mode"
-  },
-  {
-    "chinese": "正装",
-    "pinyin": "zhèng zhuāng",
-    "french": "tenue formelle"
-  },
-  {
-    "chinese": "休闲",
-    "pinyin": "xiū xián",
-    "french": "décontracté"
-  },
-  {
-    "chinese": "清新",
-    "pinyin": "qīng xīn",
-    "french": "style de fraîcheur"
-  },
-  {
-    "chinese": "脱",
-    "pinyin": "tuō",
-    "french": "enlever"
-  },
-  {
-    "chinese": "试穿",
-    "pinyin": "shì chuān",
-    "french": "essayer"
-  },
-  {
-    "chinese": "换",
-    "pinyin": "huàn",
-    "french": "changer"
-  },
-];
+  { chinese: "领带", pinyin: "lǐng dài", french: "cravate" },
+  { chinese: "棉", pinyin: "mián", french: "coton" },
+  { chinese: "毛", pinyin: "máo", french: "laine" },
+  { chinese: "丝绸", pinyin: "sī chóu", french: "soie" },
+  { chinese: "皮", pinyin: "pí", french: "cuir" },
+  { chinese: "合身", pinyin: "hé shēn", french: "ajusté" },
+  { chinese: "紧", pinyin: "jǐn", french: "moulant" },
+  { chinese: "时髦", pinyin: "shí máo", french: "à la mode" },
+  { chinese: "正装", pinyin: "zhèng zhuāng", french: "tenue formelle" },
+  { chinese: "休闲", pinyin: "xiū xián", french: "décontracté" },
+  { chinese: "清新", pinyin: "qīng xīn", french: "style de fraîcheur" },
+  { chinese: "脱", pinyin: "tuō", french: "enlever" },
+  { chinese: "试穿", pinyin: "shì chuān", french: "essayer" },
+  { chinese: "换", pinyin: "huàn", french: "changer" },
+  { chinese: "洗", pinyin: "xǐ", french: "laver" },
+]

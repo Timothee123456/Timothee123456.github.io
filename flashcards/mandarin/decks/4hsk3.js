4861ca9648032791c19a4743ad7e8525a4088a88
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-04-18 17:35:45 +0800

    Create 4hsk3.js

---
+++ flashcards/mandarin/decks/4hsk3.js
@@ -0,0 +1,21 @@
+export var title = "Lesson 4 - HSK 3";
+export var date = "2026-04-21";
+
+export var flashcards = [
+  { chinese: "比赛", pinyin: "bǐsài", french: "compétition, match" },
+  { chinese: "照片", pinyin: "zhàopiàn", french: "photo" },
+  { chinese: "年级", pinyin: "niánjí", french: "classe (scolaire)" },
+  { chinese: "又", pinyin: "yòu", french: "de nouveau, encore" },
+  { chinese: "聪明", pinyin: "cōngming", french: "intelligent" },
+  { chinese: "热情", pinyin: "rèqíng", french: "enthousiaste, chaleureux" },
+  { chinese: "努力", pinyin: "nǔlì", french: "travailler dur, faire des efforts" },
+  { chinese: "总是", pinyin: "zǒngshì", french: "toujours" },
+  { chinese: "回答", pinyin: "huídá", french: "répondre" },
+  { chinese: "站", pinyin: "zhàn", french: "se tenir debout, station" },
+  { chinese: "饿", pinyin: "è", french: "avoir faim" },
+  { chinese: "超市", pinyin: "chāoshì", french: "supermarché" },
+  { chinese: "蛋糕", pinyin: "dàngāo", french: "gâteau" },
+  { chinese: "年轻", pinyin: "niánqīng", french: "jeune" },
+  { chinese: "认真", pinyin: "rènzhēn", french: "sérieux, consciencieux" },
+  { chinese: "客人", pinyin: "kèrén", french: "invité, client" },
+];

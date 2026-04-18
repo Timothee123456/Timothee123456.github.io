ca872eb92d0242d2cc6912b7d921edbd403e0ed1
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-04-18 17:37:02 +0800

    Create 5hsk3.js

---
+++ flashcards/mandarin/decks/5hsk3.js
@@ -0,0 +1,19 @@
+export var title = "Lesson 5 - HSK 3";
+export var date = "2026-04-22";
+
+export var flashcards = [
+  { chinese: "发烧", pinyin: "fāshāo", french: "avoir de la fièvre" },
+  { chinese: "为", pinyin: "wèi", french: "pour, afin de" },
+  { chinese: "照顾", pinyin: "zhàogù", french: "prendre soin de" },
+  { chinese: "用", pinyin: "yòng", french: "utiliser" },
+  { chinese: "感冒", pinyin: "gǎnmào", french: "attraper un rhume" },
+  { chinese: "季节", pinyin: "jìjié", french: "saison" },
+  { chinese: "当然", pinyin: "dāngrán", french: "bien sûr, évidemment" },
+  { chinese: "春天", pinyin: "chūntiān", french: "printemps" },
+  { chinese: "草", pinyin: "cǎo", french: "herbe" },
+  { chinese: "夏天", pinyin: "xiàtiān", french: "été" },
+  { chinese: "裙子", pinyin: "qúnzi", french: "jupe, robe" },
+  { chinese: "最近", pinyin: "zuìjìn", french: "récemment, ces derniers temps" },
+  { chinese: "越", pinyin: "yuè", french: "plus... plus..." },
+  { chinese: "张", pinyin: "zhāng", french: "classificateur pour objets plats et larges" }
+];

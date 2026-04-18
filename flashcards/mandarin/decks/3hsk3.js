3e08a15981682480cc5687a77907cab60816beb8
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-04-18 17:33:57 +0800

    Add HSK 3 Lesson 3 flashcards

---
+++ flashcards/mandarin/decks/3hsk3.js
@@ -0,0 +1,22 @@
+export var title = "Lesson 3 - HSK 3";
+export var date = "2026-04-18";
+
+export var flashcards = [
+  { chinese: "还是", pinyin: "háishi", french: "ou bien, encore" },
+  { chinese: "爬山", pinyin: "páshān", french: "faire de la randonnée, escalader une montagne" },
+  { chinese: "小心", pinyin: "xiǎoxīn", french: "attention, prudent" },
+  { chinese: "条", pinyin: "tiáo", french: "classificateur pour objets longs et fins" },
+  { chinese: "裤子", pinyin: "kùzi", french: "pantalon" },
+  { chinese: "记得", pinyin: "jìde", french: "se souvenir" },
+  { chinese: "衬衫", pinyin: "chènshān", french: "chemise" },
+  { chinese: "元", pinyin: "yuán", french: "yuan (monnaie chinoise)" },
+  { chinese: "新鲜", pinyin: "xīnxiān", french: "frais" },
+  { chinese: "甜", pinyin: "tián", french: "doux" },
+  { chinese: "只", pinyin: "zhǐ", french: "seulement, ne...que" },
+  { chinese: "放", pinyin: "fàng", french: "poser, mettre" },
+  { chinese: "饮料", pinyin: "yǐnliào", french: "boisson" },
+  { chinese: "或者", pinyin: "huòzhě", french: "ou bien" },
+  { chinese: "舒服", pinyin: "shūfu", french: "confortable" },
+  { chinese: "花", pinyin: "huā", french: "fleur" },
+  { chinese: "绿", pinyin: "lǜ", french: "vert" }
+];

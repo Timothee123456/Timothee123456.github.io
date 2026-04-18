276687344099ae7c84db21eb71d6fb003e6c649a
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-04-18 17:32:31 +0800

    Create 2hsk3.js

---
+++ flashcards/mandarin/decks/2hsk3.js
@@ -0,0 +1,24 @@
+export var title = "Lesson 2 - HSK 3";
+export var date = "2026-04-19";
+
+export var flashcards = [
+  { chinese: "腿", pinyin: "tuǐ", french: "jambe" },
+  { chinese: "疼", pinyin: "téng", french: "douloureux, avoir mal" },
+  { chinese: "脚", pinyin: "jiǎo", french: "pied" },
+  { chinese: "树", pinyin: "shù", french: "arbre" },
+  { chinese: "容易", pinyin: "róngyì", french: "facile" },
+  { chinese: "难", pinyin: "nán", french: "difficile" },
+  { chinese: "太太", pinyin: "tàitai", french: "madame, épouse" },
+  { chinese: "秘书", pinyin: "mìshū", french: "secrétaire" },
+  { chinese: "经理", pinyin: "jīnglǐ", french: "directeur, manager" },
+  { chinese: "办公室", pinyin: "bàngōngshì", french: "bureau (pièce)" },
+  { chinese: "辆", pinyin: "liàng", french: "classificateur pour véhicules" },
+  { chinese: "楼", pinyin: "lóu", french: "immeuble, étage" },
+  { chinese: "拿", pinyin: "ná", french: "prendre, tenir" },
+  { chinese: "把", pinyin: "bǎ", french: "classificateur pour objets avec une poignée, ou pour une poignée de choses" },
+  { chinese: "伞", pinyin: "sǎn", french: "parapluie" },
+  { chinese: "胖", pinyin: "pàng", french: "gros" },
+  { chinese: "其实", pinyin: "qíshí", french: "en fait, en réalité" },
+  { chinese: "瘦", pinyin: "shòu", french: "mince, maigre" },
+  { chinese: "周", pinyin: "zhōu", french: "semaine" },
+];

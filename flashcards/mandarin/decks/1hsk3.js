a3e4864c94346c96da9e06e175a6007f99f034ab
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-04-18 17:23:29 +0800

    Create 1hsk3.js

---
+++ flashcards/mandarin/decks/1hsk3.js
@@ -0,0 +1,15 @@
+export var title = "Day 1 - HSK 3";
+export var date = "2026-04-18";
+
+export var flashcards = [
+  { chinese: "地图", pinyin: "dìtú", french: "carte" },
+  { chinese: "复习", pinyin: "fùxí", french: "réviser" },
+  { chinese: "着急", pinyin: "zháojí", french: "être inquiet, anxieux" },
+  { chinese: "搬", pinyin: "bān", french: "déménager, transporter" },
+  { chinese: "打算", pinyin: "dǎsuàn", french: "avoir l'intention de" },
+  { chinese: "一直", pinyin: "yìzhí", french: "tout le temps, toujours" },
+  { chinese: "南方", pinyin: "nánfāng", french: "sud, partie sud" },
+  { chinese: "北方", pinyin: "běifāng", french: "nord, partie nord" },
+  { chinese: "带", pinyin: "dài", french: "emmener, apporter" },
+  { chinese: "游戏", pinyin: "yóuxì", french: "jeu" }
+];

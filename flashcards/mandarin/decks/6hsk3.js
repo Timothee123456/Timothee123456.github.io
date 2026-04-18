fd6da90452758f1f2fdddb9a87481189d2dd7276
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-04-18 17:41:23 +0800

    Create 6hsk3.js

---
+++ flashcards/mandarin/decks/6hsk3.js
@@ -0,0 +1,20 @@
+export var title = "Lesson 6 - HSK 3";
+export var date = "2026-04-23";
+
+export var flashcards = [
+  { chinese: "眼镜", pinyin: "yǎnjìng", french: "lunettes" },
+  { chinese: "突然", pinyin: "tūrán", french: "soudain, brusquement" },
+  { chinese: "离开", pinyin: "líkāi", french: "quitter, partir" },
+  { chinese: "清楚", pinyin: "qīngchu", french: "clair, net" },
+  { chinese: "刚才", pinyin: "gāngcái", french: "à l'instant, juste avant" },
+  { chinese: "帮忙", pinyin: "bāngmáng", french: "aider" },
+  { chinese: "特别", pinyin: "tèbié", french: "spécial, particulièrement" },
+  { chinese: "讲", pinyin: "jiǎng", french: "parler, raconter, expliquer" },
+  { chinese: "明白", pinyin: "míngbai", french: "comprendre" },
+  { chinese: "锻炼", pinyin: "duànliàn", french: "faire de l'exercice physique" },
+  { chinese: "音乐", pinyin: "yīnyuè", french: "musique" },
+  { chinese: "公园", pinyin: "gōngyuán", french: "parc" },
+  { chinese: "聊天", pinyin: "liáotiān", french: "bavarder, discuter" },
+  { chinese: "睡着", pinyin: "shuìzháo", french: "s'endormir" },
+  { chinese: "更", pinyin: "gèng", french: "encore plus" }
+];

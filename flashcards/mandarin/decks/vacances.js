b0697491310c5e7a075893c7ca58204be35009e4
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-05-04 18:42:09 +0800

    Create vacances.js

---
+++ flashcards/mandarin/decks/vacances.js
@@ -0,0 +1,13 @@
+export var title = "Les Vacances"
+export var date = "2026-05-04"
+
+export var flashcards = [
+{ chinese: "逛逛", pinyin: "guàng guàng", french: "se balader" },
+{ chinese: "参观", pinyin: "cān guān", french: "visiter" },
+{ chinese: "博物馆", pinyin: "bó wù guǎn", french: "musée" },
+{ chinese: "休息", pinyin: "xiū xi", french: "se reposer" },
+{ chinese: "了解", pinyin: "liǎo jiě", french: "connaître" },
+{ chinese: "商店", pinyin: "shāng diàn", french: "magasin" },
+{ chinese: "越", pinyin: "yuè", french: "plus" },
+{ chinese: "等于", pinyin: "děng yú", french: "égal" }
+];

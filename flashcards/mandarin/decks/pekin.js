3196dfbd9db3b27a9a17139881bb2d2433b08f60
Timothee123456 <115542683+Timothee123456@users.noreply.github.com>
2026-09-07 13:20:39 +0800

    Create pekin.js

---
+++ flashcards/mandarin/decks/pekin.js
@@ -0,0 +1,11 @@
+export var title = "Monuments de Pékin";
+export var date = "2026-09-07";
+
+export var flashcards = [
+    {chinese: "故宫", pinyin: "Gù gōng", french: "la Cité interdite / le Palais musée"},
+    {chinese: "颐和园", pinyin: "Yí hé yuán", french: "le Palais d'Été"},
+    {chinese: "长城", pinyin: "Cháng chéng", french: "la Grande Muraille"},
+    {chinese: "天安门广场", pinyin: "Tiān'ān mén guǎng chǎng", french: "la place Tian'anmen"},
+    {chinese: "故宫也叫紫禁城，是以前皇帝住的地方。", pinyin: "Gù gōng yě jiào Zǐ jìn chéng, shì yǐ qián huáng dì zhù de dì fang.", french: "La Cité interdite est aussi appelée la Cité pourpre interdite, c'est l'endroit où habitaient les anciens empereurs."},
+    {chinese: "天安门广场是世界上最大的广场之一。", pinyin: "Tiān'ān mén guǎng chǎng shì shì jiè shang zuì dà de guǎng chǎng zhī yī.", french: "La place Tian'anmen est l'une des plus grandes places du monde."}
+];
